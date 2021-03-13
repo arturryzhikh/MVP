9f9bbce9187d33609094b06bf428b1a50a9dee9e
Artur <ryzhiha@gmail.com>
2021-03-13 16:32:30 +0300

    Initial Commit

---
+++ MVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by Artur Ryzhikh on 13.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
